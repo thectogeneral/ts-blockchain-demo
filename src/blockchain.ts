a1e555ea23f3abdad7492aafb1b1f9ffbeb88596
thectogeneral <salemolorundare@gmail.com>
2024-08-23 18:28:27 +0100

    feat:implemented blockchain validity

--- src/blockchain.ts
+++ src/blockchain.ts
@@ -25,6 +25,20 @@ class Blockchain {
         this.chain.push(newBlock);
     }
 
+    isValid(): boolean {
+        if (this.chain.length === 1) return true;
+        for(let index = 1; index < this.chain.length; index++) {
+            const currentBlock = this.chain[index];
+            const previousBlock = this.chain[index = 1];
+        if(
+            currentBlock.hash !== calculateHash(currentBlock) ||
+            previousBlock.hash !== currentBlock.previousHash
+        )
+        return false;
+    }
+    return true;
+}
+
 }
 
 export default Blockchain; 
